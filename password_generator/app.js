d19ba209743b56d54de2d08437cf50a1ad26c624
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-04 23:24:04 +0530

    project done

--- password_generator/app.js
+++ password_generator/app.js
@@ -4,15 +4,18 @@ const strengthColor = document.getElementById("strength_indicator");
 const btn = document.getElementById("generate_btn");
 const upper = document.getElementById("uppercase");
 const lower = document.getElementById("lowercase");
-const number = document.getElementById("number");
-const symbol = document.getElementById("symbol");
+const number = document.getElementById("numbers");
+const symbol = document.getElementById("symbols");
 const output = document.getElementById("show_pass");
+const copyConfirmation = document.getElementById("passwordCopied");
+const copy_btn = document.getElementById("copy_btn");
+const allCheckBox = document.querySelectorAll(".checkbox");
 let password_length = 0;
+let final_passwor = "";
 console.dir(length);
 const symbols = `"~!,./';[]=-@#$%^&*()_+<>?":{}|/*-+`;
 
 function handleSlider() {
-  console.log(length.value);
   password_length = length.value;
   showLen.innerHTML = password_length;
 }
@@ -32,21 +35,67 @@ function getRandomSymbol() {
   return symbols[getRandomInt(0, symbols.length)];
 }
 console.log(getRandomSymbol());
+
+function calcStrength() {
+  let isUpper = false;
+  let isLower = false;
+  let isSymbol = false;
+  let isNumber = false;
+
+  if (upper.checked) isUpper = true;
+  if (lower.checked) isLower = true;
+  if (symbol.checked) isSymbol = true;
+  if (number.checked) number = true;
+}
+async function copyPassword() {
+  try {
+    await navigator.clipboard.writeText(output.textContent);
+  } catch (error) {
+    alert("copy failed");
+  }
+
+  copyConfirmation.style.opacity = "1";
+  setTimeout(() => {
+    copyConfirmation.style.opacity = "0";
+  }, 2000);
+}
+copy_btn.addEventListener("click", () => {
+  if (output.textContent != "") {
+    copyPassword();
+  }
+});
+function handleCheckCount() {
+  let count = 0;
+  allCheckBox.forEach((box) => {
+    if (box.checked) count++;
+    // console.log(count);
+  });
+}
+function handleCheckChange() {
+  allCheckBox.forEach((box) => {
+    box.addEventListener("change", handleCheckCount);
+  });
+}
+handleCheckChange();
 btn.addEventListener("click", () => {
-  let finalPassword = "";
-  for (let i = 0; i < Math.ceil(password_length / 4); i++) {
-    let a = getRandomNumber();
-    let b = getRandomLower();
-    let c = getRandomSymbol();
-    let d = getRandomUpper();
-    finalPassword += a + b + c + d;
+  final_passwor = "";
+  // if (upper.checked) final_password += getRandomUpper();
+  // if (lower.checked) final_password += getRandomLower();
+  // if (symbol.checked) final_password += getRandomSymbol();
+  // if (number.checked) final_password += getRandomNumber();
+
+  const functionArray = [];
+  if (upper.checked) functionArray.push(getRandomUpper);
+  if (lower.checked) functionArray.push(getRandomLower);
+  if (symbol.checked) functionArray.push(getRandomSymbol);
+  if (number.checked) functionArray.push(getRandomNumber);
+  for (let i = 0; i < functionArray.length; i++) {
+    final_passwor += functionArray[i]();
   }
-  output.innerText = finalPassword.substring(0, password_length);
-  if (password_length > 5) {
-    strengthColor.style.background = "green";
-    strengthColor.style.boxShadow = "  2px 2px 10px 8px green";
-  } else {
-    strengthColor.style.background = "red";
-    strengthColor.style.boxShadow = "  2px 2px 10px 8px red";
+  for (let i = 0; i < password_length - functionArray.length; i++) {
+    let ind = Math.floor(Math.random() * functionArray.length + 1);
+    final_passwor += functionArray[ind]();
   }
+
+  console.log(final_passwor);
 });
